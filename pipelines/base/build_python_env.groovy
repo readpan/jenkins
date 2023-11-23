68474d079ae84d6b02003c5931d7f8b09901d828
pan <readpan@gmail.com>
2023-11-23 14:38:54 +0800

    modify

---
+++ pipelines/base/build_python_env.groovy
@@ -0,0 +1,22 @@
+// scriptA.groovy
+
+node {
+    // 定义环境变量和参数
+    def envName = 'jenkins_env'
+    def requirementsPath = 'requirements.txt'
+
+    stage('Setup Python Environment') {
+        // 检查 conda 环境是否存在
+        def envExists = sh(script: "conda info --envs | grep ${envName}", returnStatus: true) == 0
+        if (!envExists) {
+            // 创建 conda 环境
+            sh "conda create -n ${envName} python=3.10 -y"
+        }
+
+        // 激活环境并安装依赖
+        sh """
+            source activate ${envName}
+            pip install -r ${requirementsPath}
+        """
+    }
+}
