369a9f4f62fbb3a8a7a891ccb7003da825ec69c3
pan <readpan@gmail.com>
2023-11-23 15:44:11 +0800

    m

--- groovy/RunCmd.groovy
+++ groovy/RunCmd.groovy
@@ -1,26 +1,19 @@
-// RunCmd.groovy
-
-// 定义一个类
 class RunCmd {
-    
-    // 保存传入的脚本引用
+
     def script
 
-    // 构造函数
     RunCmd(script) {
         this.script = script
     }
 
-    // 方法用于运行命令
-    def runCmd(String command) {
+    def boolean runCmd(String command) {
         if (isWindows()) {
-            script.bat command
+            script.bat script: command, returnStatus: returnStatus
         } else {
-            script.sh command
+            script.sh script: command, returnStatus: returnStatus
         }
     }
 
-    // 方法用于判断是否是Windows系统
     private boolean isWindows() {
         return script.env.PATH.contains('C:\\')
     }
--- pipelines/base/build_python_env.groovy
+++ pipelines/base/build_python_env.groovy
@@ -1,22 +1,23 @@
-// 定义环境变量和参数
-def envName = 'jenkins_env'
-def requirementsPath = 'requirements.txt'
+def call(RunCmd runCmd) {
+    def envName = 'jenkins_env'
+    def requirementsPath = 'requirements.txt'
 
-stage('Setup Python Environment') {
-    // def runCmd = load 'groovy/RunCmd.groovy'
-    // 检查 conda 环境是否存在
-    def envExists = runCmd.runCmd(script: "conda info --envs | grep ${envName}")
-    // 打印envExists
-    println envExists
-    if (!envExists) {
-        // 创建 conda 环境
-        bat "echo 'conda evn not exists'"
-        // sh "conda create -n ${envName} python=3.10 -y"
+    stage('Setup Python Environment') {
+        // 使用传入的 runCmd 实例
+        // 检查 conda 玡境是否存在
+        def envExists = runCmd.runCmd("conda info --envs | grep ${envName}")
+        // 打印envExists
+        println envExists
+        if (!envExists) {
+            // 创建 conda 环境
+            bat "echo 'conda env not exists'"
+            // sh "conda create -n ${envName} python=3.10 -y"
+        }
+
+        // 激活环境并安装依赖
+        // sh """
+        //     source activate ${envName}
+        //     pip install -r ${requirementsPath}
+        // """
     }
-
-    // 激活环境并安装依赖
-    // sh """
-    //     source activate ${envName}
-    //     pip install -r ${requirementsPath}
-    // """
 }
