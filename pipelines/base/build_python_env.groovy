35b1ac29ad8671ecb017dd6f2106f2d1466b5e2d
pan <readpan@gmail.com>
2023-11-23 15:53:25 +0800

    m

--- pipelines/base/build_python_env.groovy
+++ pipelines/base/build_python_env.groovy
@@ -5,14 +5,15 @@ def call(RunCmd runCmd) {
     stage('Setup Python Environment') {
         // 使用传入的 runCmd 实例
         // 检查 conda 玡境是否存在
-        def envExists = runCmd.runCmd("conda info --envs | grep ${envName}")
-        // 打印envExists
-        println envExists
-        if (!envExists) {
-            // 创建 conda 环境
-            bat "echo 'conda env not exists'"
-            // sh "conda create -n ${envName} python=3.10 -y"
-        }
+        runCmd.runCmd("conda info --envs")
+        // def envExists = runCmd.runCmd("conda info --envs | grep ${envName}")
+        // // 打印envExists
+        // println envExists
+        // if (!envExists) {
+        //     // 创建 conda 环境
+        //     bat "echo 'conda env not exists'"
+        //     // sh "conda create -n ${envName} python=3.10 -y"
+        // }
 
         // 激活环境并安装依赖
         // sh """
