11ae54334a418fb2647c3ff6c64083179b8ff11e
pan <readpan@gmail.com>
2023-11-23 16:28:33 +0800

    m

--- groovy/RunCmd.groovy
+++ groovy/RunCmd.groovy
@@ -1,36 +1,13 @@
-// RunCmd.groovy
-
-// 定义一个类
 class RunCmd {
-    
-    // 保存传入的脚本引用
-    def script
-
-    // 构造函数
-    RunCmd(script) {
-        this.script = script
-    }
-
-    // 方法用于运行命令
-    def runCmd(String command) {
-        if (isWindows()) {
+    static def runCmd(script, String command) {
+        if (script.isWindows()) {
             script.bat command
         } else {
             script.sh command
         }
     }
 
-    // def boolean runCmd(String command, boolean returnStatus) {
-    //     if (isWindows()) {
-    //         script.bat script: command, returnStatus: returnStatus
-    //     } else {
-    //         script.sh script: command, returnStatus: returnStatus
-    //     }
-    // }
-
-    // 方法用于判断是否是Windows系统
-    private boolean isWindows() {
+    static def isWindows(script) {
         return script.env.PATH.contains('C:\\')
     }
 }
-return new RunCmd(this)
